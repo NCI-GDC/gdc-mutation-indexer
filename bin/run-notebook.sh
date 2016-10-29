b3226a32bc57efa5e779ed9ccc50333db0a788a1
Dan Kolbman <dankolbman@gmail.com>
2016-10-29 22:52:54 +0000

    feat(es): Add elasticsearch adapter docs

--- bin/run-notebook.sh
+++ bin/run-notebook.sh
@@ -1,7 +1,8 @@
 export SPARK_HOME=~/spark
+export ELASTICSEARCH_JAR=~/spark/elasticsearch-hadoop-5.0.0/dist/elasticsearch-spark-20_2.11-5.0.0.jar
 
 export PYSPARK_DRIVER_PYTHON=jupyter
 export PYSPARK_DRIVER_PYTHON_OPTS="notebook --NotebookApp.open_browser=False --NotebookApp.ip='*' --NotebookApp.port=9099"
 export PYSPARK_PYTHON=python
 
-exec $SPARK_HOME/bin/pyspark
+exec $SPARK_HOME/bin/pyspark --jars $ELASTICSEARCH_JAR
