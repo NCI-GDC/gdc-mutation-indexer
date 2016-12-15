1acbeef5ee0e2249c47a1b71d5e157bb339a88e7
Dan Kolbman <dankolbman@gmail.com>
2016-12-15 16:38:47 +0000

    feat(builder): Add skeletons for builders

---
+++ exports/builders/case_centric.py
@@ -0,0 +1,26 @@
+import os
+import yaml
+import requests
+import json
+import logging
+logging.basicConfig()
+
+from pyspark.sql.functions import lit, col, regexp_extract
+
+from exports.builders.utils import ssm_uuid_udf
+
+
+class CaseCentricBuilder(object):
+    '''
+    Builds case-centric dataframe given case and maf dataframes
+    '''
+
+    def __init__(self, config, sqlContext):
+        self.config = config
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.sqlContext = sqlContext
+
+    def build(self):
+        '''
+        '''
+        pass
---
+++ exports/builders/gene.py
@@ -0,0 +1,26 @@
+import os
+import yaml
+import requests
+import json
+import logging
+logging.basicConfig()
+
+from pyspark.sql.functions import lit, col, regexp_extract
+
+from exports.builders.utils import ssm_uuid_udf
+
+
+class GeneBuilder(object):
+    '''
+    Builds gene dataframe from the maf dataframe
+    '''
+
+    def __init__(self, config, sqlContext):
+        self.config = config
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.sqlContext = sqlContext
+
+    def build(self):
+        '''
+        '''
+        pass
