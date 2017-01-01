299923bbf74b9618c2049760f286dd9c81cba7e8
Dan Kolbman <dankolbman@gmail.com>
2017-01-01 15:16:34 +0000

    feat(builder): Add gene centric pipeline

--- exports/builders/gene_centric.py
+++ exports/builders/gene_centric.py
@@ -5,14 +5,23 @@ import json
 import logging
 logging.basicConfig()
 
-from pyspark.sql.functions import lit, col, regexp_extract
+from pyspark.sql.functions import lit, col, struct, collect_list
 
-from exports.builders.utils import ssm_uuid_udf
+from exports.builders.utils import struct_select
+from exports.builders import MAFBuilder, CaseBuilder
 
 
 class GeneCentricBuilder(object):
     '''
     Builds gene-centric dataframe given case and maf dataframes
+
+    gene{}
+         |___ case[]
+                 |___ ssm[]
+                       |___ consequence[]
+                       |             |_____ transcript{}
+                       |                          |_____ annotation{}
+                       |___ observation[]
     '''
 
     def __init__(self, config, sqlContext):
@@ -20,7 +29,111 @@ class GeneCentricBuilder(object):
         self.logger = logging.getLogger(self.__class__.__name__)
         self.sqlContext = sqlContext
 
-    def build(self):
+    def build(self, maf_df=None):
         '''
         '''
-        pass
+        self.logger.info('Building gene_centric dataframe')
+        if maf_df is None:
+            maf_df = MAFBuilder(self.config, self.sqlContext).build()
+
+        # Build the gene from the maf
+        gene_df = maf_df.select('_case_submitter_id',
+                                *struct_select('../mappings/gene.yml'))
+        # SSM
+        ssm_df = maf_df.select('_case_submitter_id',
+                               *struct_select('../mappings/ssm.yml'))\
+                               .limit(5) # TODO: Remove this
+        # Consequence
+        stmt = (struct(
+                    struct(
+                        struct(*struct_select('../mappings/annotation.yml'))
+                            .alias('annotation'),
+                           *struct_select('../mappings/transcript.yml')
+                    ).alias('transcript')
+                ).alias('consequence'))
+
+        cons_df = maf_df.select('ssm_id', stmt)\
+                        .groupBy('ssm_id')\
+                        .agg(collect_list('consequence').alias('consequence'))
+
+        # Observation
+        obs_df = maf_df.select('ssm_id',
+                               struct(*struct_select('../mappings/observation.yml'))
+                               .alias('observation'))
+
+        # Join conequence and observation
+        cons_obs = cons_df.join(obs_df, cons_df.ssm_id == obs_df.ssm_id, 'outer')\
+                            .drop(cons_df.ssm_id)\
+                            .select('ssm_id','consequence','observation')
+
+        # Build the ssm tree
+        ssm_cons = cons_obs.join(ssm_df, ssm_df.ssm_id == cons_obs.ssm_id, 'left')\
+                        .drop(cons_obs.ssm_id)\
+                        .select('_case_submitter_id', struct('consequence','observation',*ssm_df.drop('gene_id').drop('gene_id').drop('_case_submitter_id').columns).alias('ssm'))\
+                        .groupBy('_case_submitter_id')\
+                        .agg(collect_list('ssm').alias('ssm'))
+
+        # Get cases from ES
+        case_df = CaseBuilder(self.config, self.sqlContext).build()
+
+        # Combine case with ssm tree
+        case_ssm = case_df.join(ssm_cons, case_df.submitter_id == ssm_cons._case_submitter_id, 'left')\
+                    .select('submitter_id', struct('ssm', *case_df.columns).alias('case'))
+
+        gene_centric = gene_df.join(case_ssm,
+                                    gene_df._case_submitter_id == case_ssm.submitter_id,
+                                    'inner')\
+                                .groupBy(*gene_df.columns)\
+                                .agg(collect_list('case').alias('case'))\
+                                .drop('_case_submitter_id')
+
+        self.gene_centric= gene_centric
+
+        return self
+
+    def load(self, did=None):
+        '''
+        '''
+        index = self.config.indices['gene_centric']
+        doc = self.config.index_names['gene_centric']
+        index_doc = '{}/{}'.format(index, doc)
+
+        from exports.mappers import GeneMapper
+        m = GeneMapper()
+
+        print requests.delete('http://{}:{}/{}'.format(self.config.es_host,
+                                                       self.config.es_port,
+                                                       index)).json()
+        
+        data = json.dumps({"settings":{"index":{
+                        "refresh_interval":"1m",
+                        "number_of_shards":1,
+                        "number_of_replicas":0,
+                        "mapper.dynamic":False,
+                        "mapping.nested_fields.limit":100,
+                        "mapping.total_fields.limit":2000
+                    }},"mappings":{
+                        doc: m.mapping
+                    }})
+
+        print requests.put('http://{}:{}/{}'.format(self.config.es_host,
+                                                    self.config.es_port,
+                                                    index), data=data).json()
+
+        to_load = self.gene_centric
+        if did:
+            to_load = to_load.where(to_load.gene_id == did)
+
+        self.logger.info('Exporting gene centric index')
+        to_load.coalesce(1).write.format('org.elasticsearch.spark.sql')\
+                            .option('es.nodes', '{}:{}'.format(self.config.es_host, self.config.es_port))\
+                            .option('es.nodes.resolve.hostname','false')\
+                            .option('es.resource.write', index_doc)\
+                            .option('es.http.timeout', '10m')\
+                            .option('es.http.retries', '-1')\
+                            .option('es.batch.write.retry.count','-1')\
+                            .option('es.batch.write.retry.wait', '10m')\
+                            .option('es.batch.size.bytes','500mb')\
+                            .option('es.batch.size.entries', '1')\
+                            .option('es.mapping.id','gene_id')\
+                            .save(index_doc)
---
+++ tests/test_gene_centric.py
@@ -0,0 +1,76 @@
+import os
+import unittest
+import pytest
+import json
+from jsonpath_rw import parse
+from elasticsearch import Elasticsearch
+
+from conftest import get_validation_paths
+from config import TestConfig
+
+from exports.builders import GeneCentricBuilder, MAFBuilder
+
+conf = TestConfig()
+
+@pytest.yield_fixture(scope='module')
+def gene_centric_index(sqlContext, test_index):
+    ''' Generates a gene centricindex for testing '''
+    es = Elasticsearch(conf.es_host, port=conf.es_port)
+
+    r = es.indices.create(index=conf.indices['gene_centric'], ignore=400)
+
+    maf_builder = MAFBuilder(conf, sqlContext)
+    urls = ['file://'+os.path.join(conf.data_dir, 'kirp.mutect.test.maf'),
+            'file://'+os.path.join(conf.data_dir, 'kirp.muse.test.maf')]
+
+    df = maf_builder.combine(urls)
+    df = maf_builder.standardize_schema(df)
+    df = maf_builder.add_ssm_id(df)
+    df = maf_builder.extract_barcode(df)
+
+    GeneCentricBuilder(conf, sqlContext).build(df).load(did='ENSG00000092931')
+
+    yield es
+
+    if not conf.keep_indices:
+        es.indices.delete(index=conf.indices['gene_centric'], ignore=399)
+
+def flatten_json(d):
+    flat = {}
+
+    def flatten(doc, name=''):
+        if type(doc) is dict:
+            for k,v in doc.items():
+                flatten(v, name+'.'+k)
+        elif type(doc) is list:
+            for v in doc:
+                flatten(v, name)
+        else:
+            flat[name] = doc
+    flatten(d)
+    return [k for k in sorted(flat.keys(), key=lambda x: len(x)) if 'files' not in k]
+
+@pytest.mark.parametrize('doc,path',
+    get_validation_paths('tests/data/gene.validation.ENSG00000092931.json')[:1]
+)
+def test_gene_doc_contains(gene_centric_index, doc, path):
+    ''' Test that document contains a field from a path'''
+    d = gene_centric_index.get(conf.indices['gene_centric'],
+                             doc,
+                             doc_type=conf.index_names['gene_centric'])
+    d = d['_source']
+    #print json.dumps(flatten_json(d), indent=2)
+    #print 'gene' in d
+    results = parse(path).find(d)
+    assert len([r.value for r in results]) > 0
+
+@pytest.mark.parametrize('doc,path,count', [
+    ('1bf54408-b5cb-45dc-ad03-ef2866a0ff59', '[*].ssm.[*].ssm_id', 5)
+])
+def test_path_count(gene_centric_index, doc, path, count):
+    d = gene_centric_index.get(conf.indices['gene_centric'],
+                             doc,
+                             doc_type=conf.index_names['gene_centric'])
+    d = d['_source']
+    results = parse(path).find(d)
+    assert len(results) == count
