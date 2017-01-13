128c256f825bde2940a8c4aa5803532a0d6639c4
Dan Kolbman <dankolbman@gmail.com>
2017-01-13 16:37:04 +0000

    feat(builder): Update config

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ from elasticsearch import Elasticsearch
 class BaseConfig(object):
     # The Spark application name
     app_name = 'GDC_Mutation_Export'
+    spark_master = 'spark://dev-master-av2-dev2-dkolbman-notebook-0:7077'
+    spark_master = 'local[*]'
 
     api_host = 'http://api.service.consul'
     signpost_host = 'http://signpost.service.consul'
@@ -18,29 +20,29 @@ class BaseConfig(object):
     # If name is None, the index will not be built
     index_names = {
         'case_centric':         'case_centric',
-        'gene_centric':         'gene_centric',
-        'ssm_centric':          'ssm_centric',
-        'ssm_ocurrence_centric':'ssm_occurrence_centric'
+        #'gene_centric':         'gene_centric',
+        #'ssm_centric':          'ssm_centric',
+        #'ssm_ocurrence_centric':'ssm_occurrence_centric'
     }
 
     # Index revision number, will be determined automatically if not specified
     revision = None
 
     # Used for loading case/graph documents from a different es cluster
-    source_es_host = 'http://elasticsearch.service.consul'
+    source_es_host = 'http://elasticsearchvis.service.consul'
     source_es_port = 9200
     graph_document = 'case'
-    graph_index = 'gdc_from_graph'
+    graph_index = 'gdc_from_graph_5'
 
     # Namespace for ssm_ids so that they may be reproduced
     ssm_namespace = uuid.UUID('d15296a3-38ed-412e-8ace-75e235f82f55')
 
     # The name of the combined maf file
-    maf_path = 's3a://test/combined_mafs.csv'
+    maf_path = 's3a://test/uat_mafs.csv'
     # Whether to save the maf file or discard it when done
-    maf_keep = True
+    maf_keep = False
     # Use combined maf if it already exists
-    maf_use_existing = True
+    maf_use_existing = False
     # Whether to overwrite the combined maf file if it exists
     maf_overwrite = True
 
@@ -52,6 +54,12 @@ class BaseConfig(object):
                             '*_ids',
                             'project.*',
                             'program.*',
+                            #diagnoses.state',
+                            #diagnoses.morphology',
+                            #diagnoses.tumor*',
+                            #diagnoses.days_to*',
+                            #diagnoses.primary_diagnosis',
+                            #diagnoses.classification_of_tumor',
                             'demographic.*'])
     case_arrays = ','.join(['*_ids',
                             'diagnoses',
