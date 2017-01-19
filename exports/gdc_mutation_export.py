58935d372eb35c557666d2c5ba0449c166c7bda6
Max Popov <maxpmy@gmail.com>
2017-01-19 23:48:19 +0000

    merged main

--- exports/gdc_mutation_export.py
+++ exports/gdc_mutation_export.py
@@ -16,13 +16,11 @@ class GDCMutationExport(object):
     def run_export(self, config=None):
         # Construct master MAF from all individual MAFs
         urls = ['s3a://test/258c6357-4348-4b95-a266-03f50d862d9f/TCGA.KICH.somaticsniper.c652b1a7-2c9a-4d38-b317-c401b396a73e.somatic.maf.gz']
-                #'s3a://test/4dbd29dd-8afb-40bc-afda-02b3a3e74905/TCGA.KICH.muse.c6f7baac-fc69-4b3d-8a01-3604f6ef8d24.somatic.maf.gz']
         builder = MAFBuilder(self.config, self.sqlContext)
-        df = builder.combine(urls)
-        df = builder.standardize_schema(df)
-        df = builder.add_ssm_id(df)
-        df = builder.add_null(df)
-        df = builder.extract_barcode(df)
+
+        df = builder.build()
+        print '\nPASSED MAFBuilder\n'
 
         #CaseCentricBuilder(self.config, self.sqlContext).build(df).load()
         GeneCentricBuilder(self.config, self.sqlContext).build(df).load()
+
