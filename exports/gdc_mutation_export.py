5d1127416ca09dc2baa1376eecea4bd998854d58
Dan Kolbman <dankolbman@gmail.com>
2017-01-22 02:06:23 +0000

    feat(builder): Add entry points for all pipelines

--- exports/gdc_mutation_export.py
+++ exports/gdc_mutation_export.py
@@ -1,5 +1,11 @@
 from config import BaseConfig
-from builders import MAFBuilder, CaseCentricBuilder, GeneCentricBuilder
+from builders import (
+    MAFBuilder,
+    CaseCentricBuilder,
+    GeneCentricBuilder,
+    SSMCentricBuilder,
+    SSMOccurrenceCentricBuilder
+)
 
 
 class GDCMutationExport(object):
@@ -24,5 +30,19 @@ class GDCMutationExport(object):
         df = builder.add_null(df)
         df = builder.extract_barcode(df)
 
-        #CaseCentricBuilder(self.config, self.sqlContext).build(df).load()
-        GeneCentricBuilder(self.config, self.sqlContext).build(df).load()
+        if ('case_centric' in config.index_names
+                and config.index_names['case_centric'] is not None):
+            CaseCentricBuilder(self.config, self.sqlContext).build(df).load()
+
+        if ('gene_centric' in config.index_names
+                and config.index_names['gene_centric'] is not None):
+            GeneCentricBuilder(self.config, self.sqlContext).build(df).load()
+
+        if ('ssm_centric' in config.index_names
+                and config.index_names['ssm_centric'] is not None):
+            SSMCentricBuilder(self.config, self.sqlContext).build(df).load()
+
+        if ('ssm_occurrence_centric' in config.index_names
+                and config.index_names['ssm_occurrence_centric'] is not None):
+            SSMOccurrenceCentricBuilder(self.config, self.sqlContext).build(df).load()
+
