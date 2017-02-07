285f25077e3df01b8b1b6ef1f0d5c7b1df48395d
Max Popov <maxpmy@gmail.com>
2017-02-07 19:19:17 +0000

    Flat test implemented. Slightly refactored

--- tests/utils.py
+++ tests/utils.py
@@ -19,7 +19,7 @@ class SparkTestCase(unittest.TestCase):
         self.sc._jvm.System.clearProperty("spark.driver.port")
 
 
-def match_dictionaries(dict1, dict2):
+def match_json_structure(dict1, dict2):
     for k in dict1:
         if isinstance(dict1[k], dict):
             print '\n[{}] => Dict value'.format(k)
@@ -91,3 +91,22 @@ def validate_level(a, b, path='root'):
         else:
             print "[GOOD]"
             return True
+
+
+def flatten_json(y):
+    out = {}
+
+    def flatten(x, name=''):
+        if type(x) is dict:
+            for a in x:
+                flatten(x[a], name + a + '_')
+        elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + '_')
+                i += 1
+        else:
+            out[name[:-1]] = x
+
+    flatten(y)
+    return out
