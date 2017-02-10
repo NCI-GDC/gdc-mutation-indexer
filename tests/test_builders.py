028470af8252b4e4bf028cf393bdeaad5dc6798d
Dan Kolbman <dankolbman@gmail.com>
2017-02-10 21:58:05 +0000

    fix(builder): Fix tests for new mafs

--- tests/test_builders.py
+++ tests/test_builders.py
@@ -1,4 +1,5 @@
 import os
+import uuid
 import json
 import yaml
 import pytest
@@ -7,7 +8,7 @@ from collections import Counter
 
 from utils import SparkTestCase
 from exports.builders import MAFBuilder
-from exports.builders.utils import ssm_uuid, ssm_label, flat_fields, struct_select
+from exports.builders.utils import ssm_uuid, ssm_label, flat_fields, struct_select, _udf_uuid5_field
 from config import TestConfig
 
 
@@ -26,11 +27,11 @@ class TestMAFBuilder(SparkTestCase):
         builder= MAFBuilder(TestConfig(), self.sqlContext)
 
         df = builder.combine(TestConfig().maf_urls)
-        self.assertEqual(df.count(), 86298)
+        self.assertEqual(df.count(), 18)
         c = Counter([json.loads(item)['variant_caller'] for item
                      in df.select('variant_caller').toJSON().collect()])
-        self.assertEqual(c['mutect'], 52451)
-        self.assertEqual(c['muse'], 33847)
+        self.assertEqual(c['mutect'], 11)
+        self.assertEqual(c['muse'], 7)
 
     def test_schema(self):
         '''
@@ -59,6 +60,8 @@ class TestMAFBuilder(SparkTestCase):
         df = builder.add_ssm_id(df)
         self.assertIn('ssm_id', df.columns)
 
+        
+
     def test_case_barcode(self):
         '''
         Test that ssm_id column is created
@@ -70,27 +73,30 @@ class TestMAFBuilder(SparkTestCase):
         df = builder.extract_barcode(df)
 
         self.assertIn('_case_submitter_id', df.columns)
-        self.assertEqual(df.where(df.tumor_sample_barcode=='TCGA-BQ-7059-01A-11D-1961-08')\
+        self.assertEqual(df.where(df.tumor_sample_barcode=='TCGA-A4-A6HP-01A-11D-A31X-10')\
                            .select('_case_submitter_id').limit(1).collect()[0]._case_submitter_id,
-                           'TCGA-BQ-7059')
+                           'TCGA-A4-A6HP')
 
 class TestBuilderUtils(unittest.TestCase):
 
     def test_ssm_label(self):
         ''' Test ssm label generation '''
         label = ssm_label('chr3','SNP',41589825,'','A','T')
-        self.assertEqual(label, '3:g.41589825A>T')
+        self.assertEqual(label, 'chr3:g.41589825A>T')
 
         label = ssm_label('chr3','DEL',41589825,'','A','')
-        self.assertEqual(label, '3:g.41589825delA')
+        self.assertEqual(label, 'chr3:g.41589825delA')
 
         label = ssm_label('chr3','INS',41589825,41589825,'','T')
-        self.assertEqual(label, '3:g.41589825_41589825insT')
+        self.assertEqual(label, 'chr3:g.41589825_41589825insT')
+
+        label = ssm_label('chr4','SNP',112382545,'','A','T')
+        self.assertEqual(label, 'chr4:g.112382545A>T')
 
     def test_ssm_uuid(self):
         ''' Test ssm_id generation '''
-        ssm_id = ssm_uuid(TestConfig.ssm_namespace,'chr3','SNP',41589825,'','A','T')
-        self.assertEqual(ssm_id, '23f64415-9f13-5854-8450-b6ab83a9911b')
+        ssm_id = _udf_uuid5_field('ssm', 'GRCh38','chr4','112382545','112382545','SNP','A','T')
+        self.assertEqual(ssm_id, '3439eab1-0c63-50cd-bad7-1ae8ffa8aa01')
 
     def test_flat_fields(self):
         ''' Test mapping field flattener '''
