f54d1b2994429c18278ed89a72ddb19cc2a1c0e0
Thanh Nguyen <thanh.nguyen@uclouvain.be>
2017-02-26 15:25:24 -0600

    fix(ssm): fix ssm_centric

--- exports/builders/ssm_centric.py
+++ exports/builders/ssm_centric.py
@@ -80,6 +80,7 @@ class SSMCentricBuilder(BaseBuilder):
                         .groupby('ssm_id')\
                         .agg(collect_list('occurrence').alias('occurrence'))
         self.log_count(occurrence_df)
+        return occurrence_df
 
     def load(self, did=None):
         '''
