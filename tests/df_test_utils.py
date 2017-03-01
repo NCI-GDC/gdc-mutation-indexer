c7ce47a8045178416a8ccf6b84e288f5b3a191f1
Thanh Nguyen <thanh.nguyen@uclouvain.be>
2017-03-01 15:00:22 -0600

    fix(test): remove redundant function

--- tests/df_test_utils.py
+++ tests/df_test_utils.py
@@ -5,16 +5,12 @@ def validate_consequence_list_in_dept(address, list_transcripts, other_list_tran
     return validate_two_list_jsons(address, list_transcripts, other_list_transcripts,
                                    ["transcript_id"],
                                    object_name="transcript",
-                                   diff_func=validate_two_consequence)
+                                   diff_func=validate_two_nested_jsons)
 
 
 def validate_consequence_join(address, list_transcripts, other_list_transcripts):
     return validate_two_list_jsons(address, list_transcripts, other_list_transcripts,
                                    ["transcript_id"],
                                    object_name="transcript",
-                                   diff_func=validate_two_consequence,
+                                   diff_func=validate_two_nested_jsons,
                                    join_only=True)
-
-
-def validate_two_consequence(address, json_obj, other_json_obj):
-    return validate_two_nested_jsons(address, json_obj, other_json_obj)
