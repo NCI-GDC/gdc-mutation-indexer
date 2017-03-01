d0a5cf7b64df2f50643aa8d222fdd21bd0bd220b
Max Popov <maxpmy@gmail.com>
2017-03-01 00:03:28 +0000

    fix(gene_centric) DAT-624 Refactored gene_centric index. Fixed gene_centric join

--- exports/builders/gene_centric.py
+++ exports/builders/gene_centric.py
@@ -5,12 +5,15 @@ logging.basicConfig()
 
 from pyspark.sql.functions import lit, struct, collect_list
 
-from exports.builders.utils import struct_select
+from exports.builders.df_builders import (
+    get_gene_df,
+    build_ssm_subtree,
+)
 from exports.builders import (
     MAFBuilder,
     CaseBuilder,
     ConsequenceBuilder,
-    ObservationBuilder
+    ObservationBuilder,
 )
 from exports.builders import BaseBuilder
 from exports.mappers import GeneMapper
@@ -31,82 +34,106 @@ class GeneCentricBuilder(BaseBuilder):
 
     index_name = 'gene_centric'
 
-    def build(self, maf_df=None):
-        """
-        """
-        self.log('Building gene_centric dataframe')
+    def build_ssm(self, maf_df):
+        # Consequence
+        cons_df = ConsequenceBuilder(
+            self.config, self.sqlContext).build(maf_df)
 
-        self.log('\nBuilding GeneCentric')
-
-        if maf_df is None:
-            print '\nBuilding MAF'
-            maf_df = MAFBuilder(self.config, self.sqlContext).build()
-        self.log_count(maf_df)
-
-        self.log('\nBuilding Gene from MAF')
-        # Build the gene from the maf
-        gene_df = maf_df.select('_case_submitter_id',
-                                *struct_select('gene.yml', ignore=['transcripts']))
-        # SSM
-        ssm_df = maf_df.select('_case_submitter_id',
-                               *struct_select('ssm.yml'))\
-                               .drop_duplicates(['ssm_id'])
-
-        cons_df = ConsequenceBuilder(self.config, self.sqlContext).build(maf_df)\
-                               .drop_duplicates(['ssm_id'])
-
-        self.log('\nAggregating obs_df from MAF')
         # Observation
-        obs_df = maf_df.select('ssm_id',
-                               struct(*struct_select('observation.yml'))
-                               .alias('observation'))\
-                               .groupBy('ssm_id')\
-                               .agg(collect_list('observation').alias('observation'))
+        obs_df = ObservationBuilder(self.config, self.sqlContext).build(maf_df)
 
-        df = ssm_df.join(cons_df, ssm_df.ssm_id == cons_df.ssm_id, 'left')\
-                    .drop(cons_df.ssm_id)
+        # SSM
+        ssm_df = build_ssm_subtree(maf_df, obs_df, cons_df)
+        return ssm_df
 
-        df = df.join(obs_df, df.ssm_id == obs_df.ssm_id, 'left')\
-                    .drop(obs_df.ssm_id)
+    def build_case_with_gene_id(self, maf_df):
+        self.log('\nSelecting Gene from MAF')
+        gene_df = get_gene_df(maf_df, add_fields=['_case_submitter_id'])
 
-        df = df.select('_case_submitter_id', struct('consequence', 'observation', *ssm_df.drop('gene_id').drop('_case_submitter_id').columns).alias('ssm'))\
-                    .groupBy('_case_submitter_id')\
-                    .agg(collect_list('ssm').alias('ssm'))
-
-        # Truncate outliers
-        truncated_df = self.truncate_df_at_percentile(df, 'ssm.consequence', self.config.percentile_threshold['consequences_per_ssm'])
-        truncated_df = self.truncate_df_at_percentile(truncated_df, 'ssm.observation', self.config.percentile_threshold['observations_per_ssm'], df)
-
-        # Get genes from ES
-        self.log("Building gene_centric")
+        self.log("Building Case")
         case_df = CaseBuilder(self.config, self.sqlContext).build()
         self.log_count(case_df)
 
-        self.log('\nJoining case_df with truncated_df [left, "submitter_id"]')
-        # Combine case with ssm tree
-        case_ssm = case_df.join(truncated_df, case_df.submitter_id == truncated_df._case_submitter_id,
-                                'left')\
-                          .select('submitter_id', struct('ssm', *case_df.columns)
-                          .alias('case'))
+        self.log('Getting gene_id for each case via joining with gene_df')
+        case_gene_id = (
+                     gene_df.join(case_df,
+                                  gene_df._case_submitter_id ==
+                                  case_df.submitter_id,
+                                  'inner')
+                            .select('gene_id', *case_df.columns)
+                    )
+        return case_gene_id
+
+    def build_case_ssm(self, maf_df):
+        case_gene_id = self.build_case_with_gene_id(maf_df)
+
+        ssm_df = self.build_ssm(maf_df)
+        self.log('Aggregating ssm by _case_submitter_id and gene_id')
+        ssm_df = (
+                    ssm_df.select('gene_id',
+                                  '_case_submitter_id',
+                                  struct(*ssm_df
+                                         .drop('gene_id')
+                                         .drop('_case_submitter_id')
+                                         .columns)
+                                  .alias('ssm')
+                                  )
+                          .groupBy(['gene_id', '_case_submitter_id'])
+                          .agg(collect_list('ssm').alias('ssm'))
+                 )
+        self.log_count(ssm_df)
+
+        self.log("Joining Case+gene_id with SSM [inner, gene_id, submitter_id]")
+
+        join_condition = (
+            (case_gene_id.submitter_id == ssm_df._case_submitter_id) &
+            (case_gene_id.gene_id == ssm_df.gene_id)
+        )
+        case_ssm = (
+                    case_gene_id.join(ssm_df, join_condition, 'inner')
+                                .drop(ssm_df.gene_id)
+                                .select('_case_submitter_id',
+                                        struct('ssm',
+                                               *case_gene_id.columns)
+                                        .alias('case'))
+                    )
         self.log_count(case_ssm)
 
-        self.log('\nFinal join (gene_df and case_ssm, [inner, "submitter_id"]) ' \
-                 'and aggregation')
-        gene_centric = gene_df.join(case_ssm,
-                                    gene_df._case_submitter_id == case_ssm.submitter_id,
-                                    'inner')\
-                              .groupBy(*gene_df.columns)\
-                              .agg(collect_list('case').alias('case'))\
-                              .drop('_case_submitter_id')
+        return case_ssm
 
+    def build(self, maf_df=None):
+        """
+        """
+        self.log('\nBuilding GeneCentric')
+        if maf_df is None:
+            self.log('\nBuilding MAF')
+            maf_df = MAFBuilder(self.config, self.sqlContext).build()
+        self.log_count(maf_df)
 
+        self.log('\nSelecting Gene from MAF')
+        gene_df = get_gene_df(maf_df, add_fields=['_case_submitter_id'])
+
+        case_ssm = self.build_case_ssm(maf_df)
+
+        self.log('\nJoining Gene+Case with SSM [inner, "submitter_id"]')
+
+        gene_centric = (
+            gene_df.join(case_ssm,
+                         gene_df.gene_id == case_ssm.case.gene_id,
+                         'inner')
+                   .drop(case_ssm._case_submitter_id)
+                   .groupBy(*gene_df.columns)
+                   .agg(collect_list('case').alias('case'))
+        )
+
+        self.log_count(gene_centric)
         self.gene_centric = gene_centric
         self.log_count(self.gene_centric)
 
         self.log('Build finished')
         return self
 
-    def load(self, did=None):
+    def load(self):
         """
         """
         index = self.config.indices['gene_centric']
@@ -121,25 +148,22 @@ class GeneCentricBuilder(BaseBuilder):
                               auth=(self.config.es_user, self.config.es_pass),
                               data=data).json())
 
-        to_load = self.gene_centric
-
-        if did:
-            to_load = to_load.where(to_load.gene_id == did)
-
         self.log('Exporting gene centric index to {}'.format(index))
-        to_load.coalesce(20).write.format('org.elasticsearch.spark.sql')\
-                            .option('es.nodes', '{}:{}'.format(self.config.es_host, self.config.es_port))\
-                            .option('es.net.http.auth.user', self.config.es_user)\
-                            .option('es.net.http.auth.pass', self.config.es_pass)\
-                            .option('es.nodes.wan.only','true')\
-                            .option('es.nodes.resolve.hostname','false')\
-                            .option('es.resource.write', index_doc)\
-                            .option('es.http.timeout', '20m')\
-                            .option('es.http.retries', '-1')\
-                            .option('es.batch.write.retry.count', '-1')\
-                            .option('es.batch.write.retry.wait', '10m')\
-                            .option('es.batch.size.bytes','5mb')\
-                            .option('es.batch.size.entries', '100')\
-                            .option('es.mapping.id','gene_id')\
-                            .option('es.spark.dataframe.write.null', 'true')\
-                            .save(index_doc)
+        self.gene_centric.coalesce(20).write.format('org.elasticsearch.spark.sql')\
+                         .option('es.nodes', '{}:{}'
+                                 .format(self.config.es_host,
+                                         self.config.es_port))\
+                         .option('es.net.http.auth.user', self.config.es_user)\
+                         .option('es.net.http.auth.pass', self.config.es_pass)\
+                         .option('es.nodes.wan.only','true')\
+                         .option('es.nodes.resolve.hostname','false')\
+                         .option('es.resource.write', index_doc)\
+                         .option('es.http.timeout', '20m')\
+                         .option('es.http.retries', '-1')\
+                         .option('es.batch.write.retry.count', '-1')\
+                         .option('es.batch.write.retry.wait', '10m')\
+                         .option('es.batch.size.bytes','5mb')\
+                         .option('es.batch.size.entries', '100')\
+                         .option('es.mapping.id', 'gene_id')\
+                         .option('es.spark.dataframe.write.null', 'true')\
+                         .save(index_doc)
