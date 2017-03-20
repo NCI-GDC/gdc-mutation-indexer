58e73e28916c55b33fe0953d35c44f82acf718de
Dan Kolbman <dankolbman@gmail.com>
2017-03-20 19:11:36 +0000

    ✨ feat(observation_id): [DAT-700] Add observation_id

--- exports/builders/observation.py
+++ exports/builders/observation.py
@@ -19,10 +19,12 @@ class ObservationBuilder(BaseBuilder):
         said that a unique observation is identified by a unqiue pairing of
         tumor and normal sample uuids and an ssm uuid.
         '''
+
         obs_df = (maf_df.select('ssm_id', 'case_id',
+                                'occurrence_id', 'observation_id',
                                 struct(*struct_select('observation.yml'))
                                 .alias('observation'))
-                        .groupby('ssm_id', 'case_id')
+                        .groupby('ssm_id', 'case_id', 'occurrence_id')
                         .agg(collect_list('observation')
                              .alias('observation')))
 
