abd49dc03446562b9700e909eef96b3a68260d95
Max Popov <maxpmy@gmail.com>
2017-05-24 16:38:11 +0000

    fix(ssm_centric) [AP-24][DAT-853]:
    Fixes 0 mutations bug
    
    - Added available_variation_data from maf_df to CaseBuilder
    
    - Added path_exists tests for all builders

--- exports/builders/gene_centric.py
+++ exports/builders/gene_centric.py
@@ -106,7 +106,7 @@ class GeneCentricBuilder(BaseBuilder):
                    .select('case_id', 'gene_id'))
 
         self.log("Building Case")
-        case_df = CaseBuilder(self.config, self.sqlContext).build()
+        case_df = CaseBuilder(self.config, self.sqlContext).build(maf_df)
         self.log_count(case_df)
 
         self.log('Getting gene_id for each case via joining with gene_df')
