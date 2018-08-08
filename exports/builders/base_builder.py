d25ceb9e4e1c605a4f2f950c522520a54884b093
Tori Kraft <queenvkraft@gmail.com>
2018-08-08 16:20:33 -0500

    feat(dave-cna): start to stub out cna

--- exports/builders/base_builder.py
+++ exports/builders/base_builder.py
@@ -28,7 +28,14 @@ class BaseBuilder(object):
                                 http_auth=(self.config.es_user,
                                            self.config.es_pass))
 
+<<<<<<< Updated upstream
     
+=======
+        # TODO: move CaseBuilder here? think about it
+        # I'm missing maf. but technically everything that subclasses this one
+        # needs maf to do anything, so maybe that can go here too??
+
+>>>>>>> Stashed changes
     def build(self):
         """
         Contains the ETL logic to construct a spark dataframe of
---
+++ exports/builders/cna_centric.py
@@ -0,0 +1,114 @@
+import logging
+
+from pyspark.sql.functions import struct, collect_list
+
+from exports.builders import (
+    CaseBuilder,
+    ConsequenceBuilder,
+    ObservationBuilder
+)
+from exports.builders import BaseBuilder
+from exports.builders.df_builders import (
+    get_single_df
+)
+logging.basicConfig()
+
+
+class CNACentricBuilder(BaseBuilder):
+    """
+    CNA: Copy Number Alteration
+    Builds cna-centric dataframe given case and maf dataframes::
+
+        cna{}
+        |____ consequence[]
+        |             |_____ gene{}
+        |____ occurrence[]
+                    |_____ case{}
+                                |____ observation[]
+    """
+
+    index_name = 'cna_centric'
+    id_field = 'cna_id'
+
+    def build(self, maf_df):
+        """
+        Builds CNA Centric index
+        """
+        # Check if we should load a pre-built dataframe
+        if self.config.index_use_existing:
+            self.cna_centric_df = self.get_existing()
+            if self.cna_centric_df is not None:
+                return self
+
+        cna_df = get_single_df(input_df=maf_df,
+                               index_name=self.index_name,
+                               mapping='cna',
+                               unique_fields=['cna_id'])
+
+        cons_df = self.build_consequence(maf_df)
+
+        occurrence_df = self.build_occurrence(maf_df)
+
+        self.log('Final join SSM + Consequence + Occurrence')
+        ssm_centric = ssm_df.join(cons_df, on='ssm_id')\
+                            .join(occurrence_df, on='ssm_id')
+
+        # Truncate outliers
+        treshold = self.config.percentile_threshold['occurrences_per_ssm']
+        self.ssm_centric = self.truncate_df_at_percentile(ssm_centric,
+                                                          'occurrence',
+                                                          treshold)
+        self.log_count(self.ssm_centric)
+
+        self.log('Build finished')
+        # Check if we should save the resulting dataframe
+        if self.config.index_keep:
+            self.write(self.config.index_paths[self.index_name])
+        return self
+
+    def build_consequence(self, maf_df):
+        """
+        I have no idea what these options mean... join_gene, add_gene_aa_change?
+        """
+        cons_df = (ConsequenceBuilder(self.config, self.sqlContext)
+                   .build(input_df=maf_df,
+                          index_name=self.index_name,
+                          join_gene=True,
+                          add_gene_aa_change=True))
+        return cons_df
+
+    def build_occurrence(self, maf_df):
+        """
+        Occurrence
+        """
+        # 1. Observation
+        obs_df = build_observation(self, maf_df)
+        
+        # 2. Case
+        
+
+        self.log('Joining Cases with Observation, [right, case_id]')
+        occurrence_df = (case_df.join(obs_df, on=['case_id'], how='right')
+                         .select('ssm_id',
+                                 struct('occurrence_id',
+                                        struct('observation',
+                                               *case_df.columns).alias('case'))
+                                 .alias('occurrence'))
+                         .groupby('ssm_id')
+                         .agg(collect_list('occurrence').alias('occurrence')))
+        self.log_count(occurrence_df)
+        return occurrence_df
+
+    def build_observation(self, maf_df):
+        """
+        Observation
+        """
+        self.log('Aggregating Observation from MAF')
+        obs_df = (ObservationBuilder(self.config, self.sqlContext)
+                  .build(maf_df, self.index_name))
+        return obs_df
+
+    def build_case(self, maf_df):
+        case_df = CaseBuilder(self.config, self.sqlContext).build(maf_df)
+        self.log_count(case_df)
+        return case_df
