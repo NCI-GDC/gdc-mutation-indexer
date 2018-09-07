aaed3915746795207468d55ca315b3009d0f62b8
Max Popov <maxpmy@gmail.com>
2018-09-07 18:12:21 +0000

    chore(refactor): build script

--- exports/gdc_mutation_export.py
+++ exports/gdc_mutation_export.py
@@ -19,22 +19,22 @@ class GDCMutationExport(object):
         self.config = config
         self.sc = sc
         self.sqlContext = sqlContext
-        self.build_map = {
-            'case_centric': CaseCentricBuilder,
-            'gene_centric': GeneCentricBuilder,
-            'cnv_centric': CNVCentricBuilder,
-            'ssm_centric': SSMCentricBuilder,
-            'ssm_occurrence_centric': SSMOccurrenceCentricBuilder,
-            'cnv_occurrence_centric': CNVOccurrenceCentricBuilder,
-        }
+        self.builders = [
+            CaseCentricBuilder,
+            GeneCentricBuilder,
+            SSMCentricBuilder,
+            SSMOccurrenceCentricBuilder,
+            CNVCentricBuilder,
+            CNVOccurrenceCentricBuilder,
+        ]
 
     def run_export(self, config=None):
         # Construct master MAF from all individual MAFs
         maf_df = MAFBuilder(self.config, self.sqlContext).build()
         gistic_df = GisticBuilder(self.config, self.sqlContext).build()
 
-        for index_name, builder in sorted(self.build_map.items(),
-                                          key=lambda x: x[0]):
+        for builder in self.builders:
+            index_name = builder.index_name
             if (index_name in config.index_names
                     and config.index_names[index_name] is not None):
                 if index_name in ['ssm_centric', 'ssm_occurrence_centric']:
