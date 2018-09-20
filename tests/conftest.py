c6e9b07c13ebd3444ebe922e2cc2904702dc06c5
Tori Kraft <queenvkraft@gmail.com>
2018-09-20 16:53:31 -0500

    feat(caseBuilderRefactor): refactor tests with case_df

--- tests/conftest.py
+++ tests/conftest.py
@@ -209,14 +209,14 @@ def ssm_transcript_df(sqlContext, maf_df):
 
 
 @pytest.fixture(scope='session')
-def case_centric_df(sqlContext, maf_df, gistic_df):
+def case_centric_df(sqlContext, maf_df, gistic_df, case_df):
     """
     Builds case centric dataframe once. Loads to elasticsearch index
     Reused throughout test suite
     """
     log.info('\n\n\tBUILDING CASE_CENTRIC_DF\n\n')
     builder = CaseCentricBuilder(conf, sqlContext)
-    builder.build(maf_df, gistic_df)
+    builder.build(maf_df, gistic_df, case_df)
 
     log.info('\n\n\tLOADING CASE_CENTRIC_DF\n\n')
     builder.load()
@@ -269,13 +269,13 @@ def ssm_occurrence_centric_df(sqlContext, maf_df):
 
 
 @pytest.fixture(scope='session')
-def cnv_centric_df(sqlContext, maf_df, gistic_df):
+def cnv_centric_df(sqlContext, gistic_df, case_df):
     """
     Builds cnv centric dataframe
     """
     log.info('\n\n\tBUILDING CNV_CENTRIC DF\n\n')
     builder = CNVCentricBuilder(conf, sqlContext)
-    builder.build(maf_df, gistic_df)
+    builder.build(gistic_df, case_df)
 
     log.info('\n\n\tLOADING CNV_CENTRIC_DF\n\n')
     builder.load()
@@ -283,13 +283,13 @@ def cnv_centric_df(sqlContext, maf_df, gistic_df):
 
 
 @pytest.fixture(scope='session')
-def cnv_occurrence_centric_df(sqlContext, maf_df, gistic_df):
+def cnv_occurrence_centric_df(sqlContext, gistic_df, case_df):
     """
     Builds cnv occurrence centric dataframe
     """
     log.info('\n\n\tBUILDING CNV_OCCURRENCE_CENTRIC DF\n\n')
     builder = CNVOccurrenceCentricBuilder(conf, sqlContext)
-    builder.build(maf_df, gistic_df)
+    builder.build(gistic_df, case_df)
 
     log.info('\n\n\tLOADING CNV_OCCURRENCE_CENTRIC_DF\n\n')
     builder.load()
