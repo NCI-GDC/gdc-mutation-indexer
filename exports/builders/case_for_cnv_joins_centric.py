15f25181ca5fc0b39d27829b4747d05a7e062e60
Tori Kraft <queenvkraft@gmail.com>
2018-10-30 16:20:56 -0500

    feat(joins): case for cnv joins

--- exports/builders/case_for_cnv_joins_centric.py
+++ exports/builders/case_for_cnv_joins_centric.py
@@ -10,15 +10,15 @@ from config import LOG_FORMAT
 logging.basicConfig(format=LOG_FORMAT)
 
 
-class CaseForSSMJoinsCentricBuilder(BaseBuilder):
+class CaseForCNVJoinsCentricBuilder(BaseBuilder):
     """
-    Builds case docs for joining to SSM.
+    Builds case docs for joining to CNV.
 
         case{}
             |____ observation[]
     """
 
-    index_name = 'case_for_ssm_joins_centric'
+    index_name = 'case_for_cnv_joins_centric'
     id_field = 'case_id'
 
     def build(self, maf_df, case_df):
@@ -27,32 +27,34 @@ class CaseForSSMJoinsCentricBuilder(BaseBuilder):
         """
         # Check if we should load a pre-built dataframe
         if self.config.index_use_existing:
-            self.case_for_ssm_joins_centric = self.get_existing()
-            if self.case_for_ssm_joins_centric is not None:
+            self.case_for_cnv_joins_centric = self.get_existing()
+            if self.case_for_cnv_joins_centric is not None:
                 return self
 
-        self.case_for_ssm_joins_centric = \
-            self.build_case_for_ssm(maf_df, case_df)
+        self.case_for_cnv_joins_centric = \
+            self.build_case_for_cnv(maf_df, case_df)
 
         self.log('Build finished')
+
         # Check if we should save the resulting dataframe
         if self.config.index_keep:
             self.write(self.config.index_paths[self.index_name])
 
         return self
 
-    def build_case_for_ssm(self, maf_df, case_df):
+    def build_case_for_cnv(self, maf_df, case_df):
 
         # Observation
         self.log('Aggregating Observation from MAF')
-        obs_df = ObservationBuilder().build_for_ssm(maf_df, self.index_name)
+        obs_df = ObservationBuilder().build_for_cnv(gistic_df, self.index_name)
+        import ipdb; ipdb.set_trace()
 
         self.log('Joining Cases with Observation, [right, case_id]')
-        case_for_ssm_df = (case_df.join(obs_df,
+        case_for_cnv_df = (case_df.join(obs_df,
                                         on=['case_id'],
                                         how='right')
-                           ).drop('ssm_id').drop('occurrence_id')
+                           ).drop('cnv_id').drop('occurrence_id')
 
-        self.log_count(case_for_ssm_df)
+        self.log_count(case_for_cnv_df)
 
-        return case_for_ssm_df
+        return case_for_cnv_df
--- tests/conftest.py
+++ tests/conftest.py
@@ -30,6 +30,7 @@ from exports.builders import (
     SSMCentricBuilder,
     SSMOccurrenceCentricBuilder,
     CaseForSSMJoinsCentricBuilder,
+    CaseForCNVJoinsCentricBuilder,
 )
 
 conf = TestConfig()
@@ -300,6 +301,20 @@ def case_for_ssm_joins_centric_df(sqlContext, maf_df, case_df):
     log.info('\n\n\tLOADING case_for_ssm_joins\n\n')
     builder.load()
     return builder.case_for_ssm_joins_centric
+
+
+@pytest.fixture(scope='session')
+def case_for_cnv_joins_centric_df(sqlContext, gistic_df, case_df):
+    """
+    Builds case_for_cnv_joins centric dataframe
+    """
+    log.info('\n\n\tBUILDING case_for_cnv_joins DF\n\n')
+    builder = CaseForCNVJoinsCentricBuilder(conf, sqlContext)
+    builder.build(gistic_df, case_df)
+
+    log.info('\n\n\tLOADING case_for_cnv_joins\n\n')
+    builder.load()
+    return builder.case_for_cnv_joins_centric
 
 
 @pytest.fixture(scope='session')
