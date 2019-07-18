f423d69cca8539f5895608eeea8b231c31740ed3
Greg Singer <singergr@uchicago.edu>
2019-07-18 14:50:28 -0500

    feat(trim): trim nested summary only
    Trim the nested file summary out of the nested case documents
    in indices other than case_centric.
    
    Don't do any other trimming just yet.

--- exports/gdc_mutation_export.py
+++ exports/gdc_mutation_export.py
@@ -54,18 +54,23 @@ class GDCMutationExport(object):
         case_df = CaseBuilder(self.config,
                               self.sqlContext).build(maf_df, gistic_df)
 
+        sub_case_df = case_df.drop('summary')
+
         for builder in self.builders:
             index_name = builder.index_name
             if index_name in self.config.index_types:
                 self.sc.setJobGroup(index_name, 'Build {}'.format(index_name))
-                if index_name in ['ssm_centric', 'ssm_occurrence_centric']:
-                    # these builders do not yet depend on gistic_df
-                    builder(self.config, self.sqlContext).build(maf_df, case_df).load()
-                elif index_name in ['cnv_centric', 'cnv_occurrence_centric']:
-                    # these builders do not depend on maf_df
-                    builder(self.config, self.sqlContext).build(gistic_df, case_df).load()
-                else:
+                if index_name == 'case_centric':
                     builder(self.config,
                             self.sqlContext).build(maf_df, gistic_df, case_df).load()
+                elif index_name in ['ssm_centric', 'ssm_occurrence_centric']:
+                    # these builders do not yet depend on gistic_df
+                    builder(self.config, self.sqlContext).build(maf_df, sub_case_df).load()
+                elif index_name in ['cnv_centric', 'cnv_occurrence_centric']:
+                    # these builders do not depend on maf_df
+                    builder(self.config, self.sqlContext).build(gistic_df, sub_case_df).load()
+                else:
+                    builder(self.config,
+                            self.sqlContext).build(maf_df, gistic_df, sub_case_df).load()
 
         self.logger.info('Mutation Indexer finished successfully')
