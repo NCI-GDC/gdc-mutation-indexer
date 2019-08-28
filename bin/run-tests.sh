9249503b79b22a184dd88c822f491be22918c778
Greg Singer <singergr@uchicago.edu>
2019-08-27 19:20:25 -0500

    feat(TT-834): use spark 2.4.3 in tests/docs
    Update the docs and test setup to install and/or refer to Spark 2.4.3.
    Also update Elasticsearch to 5.6.14 to match current prod.

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export SPARK_HOME=~/spark
 export PYTHONPATH=$SPARK_HOME/python/:$PYTHONPATH
-export PYTHONPATH=$SPARK_HOME/python/lib/py4j-0.10.3-src.zip:$PYTHONPATH
+export PYTHONPATH=$SPARK_HOME/python/lib/py4j-0.10.7-src.zip:$PYTHONPATH
 
 py.test tests "$@" -vvv
