11bf0ad592c06f56ec4ef7d81d3c52438e36ed92
Greg Singer <singergr@uchicago.edu>
2019-08-28 13:52:36 -0500

    feat(TT-834): update unit tests for spark 2.4.3
    Update tests to account for changes in how Spark handles selects
    involving struct fields.
    
    With Spark 2.1.1, df.select(..., 'case.case_id') yields a column named
    'case_id'. Spark 2.4.3 can yield either 'case_id' or 'case.case_id',
    depending on what other columns are selected. The fix is to explicitly
    set the column alias we want.

--- tests/builders/base_joins_test.py
+++ tests/builders/base_joins_test.py
@@ -1,5 +1,5 @@
 import json
-from pyspark.sql.functions import explode
+from pyspark.sql.functions import col, explode
 
 
 class BaseJoinsTest:
@@ -62,16 +62,18 @@ class BaseJoinsTest:
             '{}.{}'.format(exploded_alias, f) for f in packed_fields
         ]
 
+        # this split allows deeper parent fields like "foo.bar"
         all_fields = (
-            [f.split('.')[-1] for f in parent_fields] +  # this allows deeper parent fields like "foo.bar"
+            [f.split('.')[-1] for f in parent_fields] +
             child_fields
         )
 
         unpacked = (
-            dataframe.select(explode(list_field).alias(exploded_alias),
-                             *parent_fields)
-                     .select(*all_fields)
+            dataframe.select(
+                explode(list_field).alias(exploded_alias),
+                *[col(f).alias(f.split('.')[-1]) for f in parent_fields]
+            )
+            .select(*all_fields)
         )
 
         return unpacked
-
