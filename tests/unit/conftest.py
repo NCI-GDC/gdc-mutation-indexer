14b889a2317dc178da1a8e4e8d2a67f56509eb3c
Maksim An <anmaxvl@gmail.com>
2020-08-18 23:59:45 -0500

    DEV-219: add unit tests for gene expression aliquot selection

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,33 +1,29 @@
+import os
+
+import yaml
 import pytest
 
 
 @pytest.fixture
-def fake_es_hits():
-    response = {
-        "hits": {
-            "hits": [
-                {
-                    "file_id": "file-1",
-                    "created_datetime": "2020-01-02",
-                    "cases": [
-                        {
-                            "case_id": "case-1",
-                            "samples": [{"sample_type": "Primary Tumor"}]
-                        }
-                    ]
-                },
-                {
-                    "file_id": "file-2",
-                    "created_datetime": "2020-01-01",
-                    "cases": [
-                        {
-                            "case_id": "case-1",
-                            "samples": [{"sample_type": "Primary Tumor"}]
-                        }
-                    ]
-                }
-            ]
-        }
-    }
+def data_dir():
+    current_path = os.path.dirname(os.path.abspath(__file__))
 
-    return response
+    return os.path.join(current_path, "data")
+
+
+@pytest.fixture
+def fake_hits_and_expectations(data_dir):
+
+    def load_hits_from_file(filename):
+        with open(os.path.join(data_dir, filename)) as f:
+            contents = yaml.safe_load(f)
+
+        hits = contents["hits"]
+        expected = contents["expected"]
+
+        return (
+            {"hits": [{"_id": hit.pop("_id"), "_source": hit} for hit in hits]},
+            expected,
+        )
+
+    return load_hits_from_file
--- tests/unit/test_gene_expression_builder_utils.py
+++ tests/unit/test_gene_expression_builder_utils.py
@@ -1,5 +1,12 @@
+import pytest
 from exports.builders import utils
 
 
-def test_aliquot_selection():
-    pass
+@pytest.mark.parametrize("filename", ["files-response-1.yaml"])
+def test_aliquot_selection(fake_hits_and_expectations, filename):
+    response, expected = fake_hits_and_expectations(filename)
+
+    res = utils._select_primary_aliquot_gene_expressions(response["hits"])
+
+    assert len(res) == len(expected)
+    assert set(res) == set(expected)
