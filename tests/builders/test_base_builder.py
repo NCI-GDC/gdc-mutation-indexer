e8b6db55e1929d46e9a10b78dfe7f12f0670ac2e
asurin <qiaozha@gmail.com>
2020-10-14 16:23:08 -0500

    DEV-217 fix test

--- tests/builders/test_base_builder.py
+++ tests/builders/test_base_builder.py
@@ -4,8 +4,14 @@ from collections import Counter
 import pytest
 from normalizer.mapper import ModelMapper
 
-from exports.builders import CaseCentricBuilder, GeneCentricBuilder, SSMCentricBuilder, SSMOccurrenceCentricBuilder, \
-    CNVCentricBuilder, CNVOccurrenceCentricBuilder
+from exports.builders import (
+    CaseCentricBuilder,
+    GeneCentricBuilder,
+    SSMCentricBuilder,
+    SSMOccurrenceCentricBuilder,
+    CNVCentricBuilder,
+    CNVOccurrenceCentricBuilder,
+)
 from exports.builders.base_builder import get_all_boolean_paths
 from tests_config import TestConfig
 
@@ -13,25 +19,31 @@ conf = TestConfig()
 
 
 def test_get_all_boolean_paths():
-    with open('tests/data/input/mapping.json') as mapping_fp:
+    with open("tests/data/input/mapping.json") as mapping_fp:
         data = json.load(mapping_fp)
         paths = get_all_boolean_paths(data)
-        assert paths == [[u'gene', u'cnv', u'gene_level_cn'], [u'gene', u'is_cancer_gene_census']]
+        assert paths == [
+            [u"gene", u"cnv", u"gene_level_cn"],
+            [u"gene", u"is_cancer_gene_census"],
+        ]
 
 
-@pytest.mark.parametrize("builder_class,index", ((CaseCentricBuilder, "case_centric"), (GeneCentricBuilder, "gene_centric"), (SSMCentricBuilder, "ssm_centric"), (SSMOccurrenceCentricBuilder, "ssm_occurrence_centric"), (CNVCentricBuilder, "cnv_centric"), (CNVOccurrenceCentricBuilder, "cnv_occurrence_centric")))
-def test_base_builder(sqlContext, maf_df, gistic_df, case_df, builder_class, index):
+@pytest.mark.parametrize(
+    "builder_class,index",
+    (
+        (CaseCentricBuilder, "case_centric"),
+        (GeneCentricBuilder, "gene_centric"),
+        (SSMCentricBuilder, "ssm_centric"),
+        (SSMOccurrenceCentricBuilder, "ssm_occurrence_centric"),
+        (CNVCentricBuilder, "cnv_centric"),
+        (CNVOccurrenceCentricBuilder, "cnv_occurrence_centric"),
+    ),
+)
+def test_base_builder_cast_boolean(sqlContext, builder_class, index, request):
     builder = builder_class(conf, sqlContext)
-    if index == "case_centric":
-        builder.build(maf_df, gistic_df, case_df)
-    elif index == "gene_centric":
-        builder.build(maf_df, gistic_df, case_df.drop("summary"))
-    elif index == "ssm_centric" or index == "ssm_occurrence_centric":
-        builder.build(maf_df, case_df.drop("summary"))
-    else:
-        builder.build(gistic_df, case_df.drop("summary"))
-    df = builder.load()
+    df = request.getfixturevalue("{}_df".format(index))
     index_mapper = ModelMapper(index)
+    df = builder.check_and_cast_booleans(df, index_mapper.mapping)
     paths = get_all_boolean_paths(index_mapper.mapping)
     boolean_counts = Counter(path[-1] for path in paths)
     simple_string = df.schema.simpleString()
