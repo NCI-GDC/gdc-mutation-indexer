2a7c5c3b336f009f39537cee8f7ec03ca7546114
asurin <qiaozha@gmail.com>
2021-01-04 12:14:17 -0600

    remove decode in setup.py

--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ setup(
     packages=PACKAGES,
     py_modules=["config"],
     include_package_data=True,
-    options=dict(egg_info=dict(tag_build=('_rev_' + get_git_commit(ROOT_DIR).decode('UTF-8')))),
+    options=dict(egg_info=dict(tag_build=('_rev_' + get_git_commit(ROOT_DIR)))),
 )
