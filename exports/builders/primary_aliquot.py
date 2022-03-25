51d6de25e263253b2000a59fe1d08acce3d3a5ca
BenX0 <JaNatuurlijk231@gmail.com>
2022-03-25 12:41:43 -0500

    DEV-1066: Create gene expression primary aliquot builder

--- exports/builders/primary_aliquot.py
+++ exports/builders/primary_aliquot.py
@@ -1,6 +1,6 @@
 import itertools
 import logging
-from typing import AbstractSet, Iterable, List, NamedTuple, Optional, Tuple, Union
+from typing import AbstractSet, Iterable, List, NamedTuple, Optional, Union
 
 import more_itertools
 from indexclient import client
@@ -159,23 +159,17 @@ GeneExpressionPrimaryAliquotData = NamedTuple(
 )
 
 
-class PrimaryAliquotBuilder(base_input_builder.BaseInputBuilder):
-    FILE_URL_BATCH_SIZE = 1000
-
+class BasePrimaryAliquotBuilder(base_input_builder.BaseInputBuilder):
     def __init__(
         self,
         config: config.BaseConfig,
-        sql_context: sql.SQLContext,
-        indexd: client.IndexClient,
+        sqlContext: sql.SQLContext,
         es_dataframe_util: es_utils.DataFrameUtil,
-        es_rdd_util: es_utils.RDDUtil,
+        input_type: str,
     ) -> None:
-        super().__init__(config, sql_context, "primary_aliquot")
-        self._sql_context = sql_context
-        self._indexd = indexd
+        super().__init__(config, sqlContext, input_type)
+
         self._es_dataframe_util = es_dataframe_util
-        self._es_rdd_util = es_rdd_util
-        self._logger = logging.getLogger(self.__class__.__name__)
 
     def _get_weighted_df(
         self, query: dict, include_fields: Union[Iterable[str], bool]
@@ -285,6 +279,116 @@ class PrimaryAliquotBuilder(base_input_builder.BaseInputBuilder):
                 "case",
             )
         )
+
+
+def _get_gene_expression_filters(projects: Optional[List[str]]) -> List[dict]:
+    filters = [
+        {"terms": {"data_type": ["Gene Expression Quantification"]}},
+        {"terms": {"acl": ["open"]}},
+        {"term": {"analysis.workflow_type": "STAR - Counts"}},
+    ]  # type: List[dict]
+
+    if projects:
+        filters.append(
+            {
+                "nested": {
+                    "path": "cases",
+                    "query": {"terms": {"cases.project.project_id": projects}},
+                }
+            }
+        )
+
+    return filters
+
+
+class GeneExpressionPrimaryAliquotBuilder(BasePrimaryAliquotBuilder):
+    def __init__(
+        self,
+        config: config.BaseConfig,
+        sql_context: sql.SQLContext,
+        es_dataframe_util: es_utils.DataFrameUtil,
+    ) -> None:
+        super().__init__(
+            config, sql_context, es_dataframe_util, "gene_expression_primary_aliquot"
+        )
+
+    def build_from_scratch(self, **kwargs: sql.DataFrame) -> sql.DataFrame:
+        """
+        Gets the case and it's associated file data for the mutation index.
+
+        Args:
+            workflow_types: A collection of analysis workflow types to
+                filter files on.
+
+        Returns:
+            (GeneExpressionPrimaryAliquotData): An object containing the primary aliquot
+            dataframe as well as a list of all the file urls associated with the primary
+            aliquots.
+
+            primary_aliquot{}
+            |---file_id
+            |---file_url
+            |---case_id
+            |---submitter_id
+            |---demographic{}
+            |   |---days_to_death
+            |   |---ethnicity
+            |   |---gender
+            |   |---race
+            |   |---vital_status
+            |
+            |---project{}
+            |   |---project_id
+            |
+            |---diagnoses[]
+            |   |---age_at_diagnosis
+            |
+            |---samples[]
+                |---sample_type
+        """
+        filters = _get_gene_expression_filters(self.config.projects)
+        case_fields = [
+            "cases.submitter_id",
+            "cases.demographic.days_to_death",
+            "cases.demographic.ethnicity",
+            "cases.demographic.gender",
+            "cases.demographic.race",
+            "cases.demographic.vital_status",
+            "cases.project.project_id",
+            "cases.diagnoses.age_at_diagnosis",
+        ]
+
+        return self._get_primary_aliquot_df(
+            filters,
+            entities=frozenset(("case",)),
+            include_fields=case_fields,
+        ).select(
+            "file_id",
+            "case_id",
+            "case.submitter_id",
+            "case.demographic",
+            "case.project",
+            "case.diagnoses",
+            "case.samples",
+        )
+
+
+class PrimaryAliquotBuilder(BasePrimaryAliquotBuilder):
+    FILE_URL_BATCH_SIZE = 1000
+
+    def __init__(
+        self,
+        config: config.BaseConfig,
+        sql_context: sql.SQLContext,
+        indexd: client.IndexClient,
+        es_dataframe_util: es_utils.DataFrameUtil,
+        es_rdd_util: es_utils.RDDUtil,
+    ) -> None:
+        super().__init__(config, sql_context, es_dataframe_util, "primary_aliquot")
+        self._sql_context = sql_context
+        self._indexd = indexd
+        self._es_rdd_util = es_rdd_util
+        self._logger = logging.getLogger(self.__class__.__name__)
 
     def _get_main_urls(self, file_ids: Iterable[str]) -> Iterable[sql.Row]:
         """
