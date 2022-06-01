41e8003321e1d01590436ade9286c4f5338e73d1
BenX0 <JaNatuurlijk231@gmail.com>
2022-06-01 12:40:02 -0500

    DEV-882: Switch pyspark to master requirements

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         "ndjson~=0.3",
         "networkx<=2.4",
         "more-itertools~=8.9",
+        "pyspark==2.4.5",
         "python-dateutil~=2.8",
         "PyYaml>=3.11,<6",
         "requests~=2.7",
