0b0ecf0bcbe34f2ca6c71a24e72b6edc4c82d618
BenX0 <JaNatuurlijk231@gmail.com>
2022-06-28 10:40:47 -0500

    DEV-1131: Move caching logic to base input builder

--- config.py
+++ config.py
@@ -102,8 +102,8 @@ class BaseConfig(object):
     }
 
     cache_dataframes = {
-        "mafs": True,
-        "cases": True,
+        "maf": True,
+        "case": True,
         "case_centric": True,
         "gene_centric": True,
         "ssm_centric": True,
