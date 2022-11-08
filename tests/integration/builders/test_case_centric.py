374ebe550a8dd72d3c2642b91d80b898a54a2383
Micky Byrnes <68259544+BenX00@users.noreply.github.com>
2022-11-08 11:36:52 -0600

    DEV-1342: Create test_case_centric module & remove TestConfig. (#361)

---
+++ tests/integration/builders/test_case_centric.py
@@ -0,0 +1,169 @@
+from typing import AbstractSet
+
+import pytest
+from pyspark import sql
+
+import config
+from exports import builders
+from tests.integration.utils import join_utils
+
+
+def test_genes_per_case(
+    maf_df: sql.DataFrame, cnv_df: sql.DataFrame, case_centric_df: sql.DataFrame
+) -> None:
+    # Genes per case built:
+    df = join_utils.unpack_df_list(case_centric_df, "case_id", "gene", "gene_id")
+    gpc = join_utils.get_relationship_map(df, ("case_id", "gene_id"))
+
+    # Genes per case expected:
+    df = (
+        maf_df.select("case_id", "gene_id").union(cnv_df.select("case_id", "gene_id"))
+    ).distinct()
+    true_gpc = join_utils.get_relationship_map(df, ("case_id", "gene_id"))
+
+    assert gpc == true_gpc
+
+
+def test_ssm_per_gene(maf_df: sql.DataFrame, case_centric_df: sql.DataFrame) -> None:
+    # SSMs per gene built:
+    df = join_utils.unpack_df_list(
+        case_centric_df, "case_id", "gene", ("gene_id", "ssm")
+    )
+    df = join_utils.unpack_df_list(df, ("case_id", "gene_id"), "ssm", "ssm_id")
+    es_spg = join_utils.get_relationship_map(df, ("case_id", "gene_id", "ssm_id"))
+
+    # SSMs per gene expected:
+    df = maf_df.select("gene_id", "case_id", "ssm_id")
+    spg = join_utils.get_relationship_map(df, ("case_id", "gene_id", "ssm_id"))
+
+    assert es_spg == spg
+
+
+@pytest.mark.case_centric_ssm_subtree
+@pytest.mark.usefixtures("case_centric_df")
+def test_ssm_subtree(
+    default_old_config: config.BaseConfig,
+    sqlContext: sql.SQLContext,
+    maf_df: sql.DataFrame,
+    primary_aliquot_df: sql.DataFrame,
+    case_ssm_subtree: sql.DataFrame,
+) -> None:
+    observation_builder = builders.ObservationBuilder()
+    consequence_builder = builders.ConsequenceBuilder(default_old_config, sqlContext)
+
+    # ssm_subtree stats expected:
+    cons_df = consequence_builder.build_for_ssm(maf_df, "case_centric")
+    obs_df = observation_builder.build_for_ssm(
+        maf_df, primary_aliquot_df, "case_centric", selector="ssm"
+    )
+
+    df = cons_df.join(obs_df, on=["ssm_id"], how="left")
+
+    df = join_utils.unpack_df_list(
+        df, ("ssm_id", "observation"), "consequence", "consequence_id"
+    )
+    df = join_utils.unpack_df_list(
+        df, ("ssm_id", "consequence_id"), "observation", "observation_id"
+    )
+    data = tuple(df.toLocalIterator())
+    consequences_stats = join_utils.get_relationship_map(
+        data, ("ssm_id", "consequence_id")
+    )
+    observations_stats = join_utils.get_relationship_map(
+        data, ("ssm_id", "observation_id")
+    )
+
+    # ssm_subtree stats built:
+    df = join_utils.unpack_df_list(
+        case_ssm_subtree, (), "ssm", ("ssm_id", "consequence", "observation")
+    )
+    df = join_utils.unpack_df_list(
+        df, ("ssm_id", "observation"), "consequence", "consequence_id"
+    )
+    df = join_utils.unpack_df_list(
+        df, ("ssm_id", "consequence_id"), "observation", "observation_id"
+    )
+    data = tuple(df.toLocalIterator())
+    es_consequences_stats = join_utils.get_relationship_map(
+        data, ("ssm_id", "consequence_id")
+    )
+    es_observations_stats = join_utils.get_relationship_map(
+        data, ("ssm_id", "observation_id")
+    )
+
+    assert consequences_stats == es_consequences_stats
+    assert observations_stats == es_observations_stats
+
+
+def test_case_centric_counts(
+    case_centric_df: sql.DataFrame,
+    cnv_df: sql.DataFrame,
+    all_cases: AbstractSet[str],
+    all_maf_cases: AbstractSet[str],
+) -> None:
+    """
+    Test "empty cases"
+
+    Confirm that we index cases even if they have no maf or cnv data.
+    This is necessary for the portal to visualize such cases.
+    """
+    cases_built = frozenset(
+        c.case_id
+        for c in case_centric_df.select("case_id").distinct().toLocalIterator()
+    )
+    all_cnv_cases = frozenset(
+        r.case_id for r in cnv_df.select("case_id").distinct().toLocalIterator()
+    )
+    cases_with_data = all_maf_cases | all_cnv_cases
+    empty_cases = all_cases - cases_with_data
+
+    # confirm that we have at least one empty case in our test data
+    assert empty_cases
+
+    # check that all cases were built (even empty ones)
+    assert all_cases == cases_built
+
+
+def test_available_variation_data(
+    case_centric_df: sql.DataFrame,
+    cnv_df: sql.DataFrame,
+    all_cases: AbstractSet[str],
+    all_maf_cases: AbstractSet[str],
+) -> None:
+    """
+    Test that available_variation_data is correctly populated:
+        * ['cnv'] - for cnv-only cases
+        * ['ssm'] - for cases in the maf header that do not have cnv data
+        * ['cnv', 'ssm'] - for cases that have both maf and cnv data
+        * [] - for cases that have no maf or cnv data
+    """
+
+    assert "available_variation_data" in case_centric_df.columns
+
+    gistic_cases = frozenset(
+        r.case_id for r in cnv_df.select("case_id").distinct().toLocalIterator()
+    )
+    maf_cases = all_maf_cases  # includes cases in maf header without ssms
+
+    common_cases = gistic_cases & maf_cases
+    cnv_cases = gistic_cases - maf_cases
+    ssm_cases = maf_cases - gistic_cases
+    empty_cases = all_cases - gistic_cases - maf_cases
+
+    assert common_cases, "there were no common cases found in test data"
+    assert cnv_cases, "there were no cnv cases found in test data"
+    assert ssm_cases, "there were no ssm cases found in test data"
+    assert empty_cases, "there were no empty cases found in test data"
+
+    # Check that 'available_variation_data' is populated correctly
+    for row in case_centric_df.select(
+        "case_id", "available_variation_data"
+    ).toLocalIterator():
+        if row.case_id in common_cases:
+            assert row.available_variation_data == ["cnv", "ssm"]
+        elif row.case_id in cnv_cases:
+            assert row.available_variation_data == ["cnv"]
+        elif row.case_id in ssm_cases:
+            assert row.available_variation_data == ["ssm"]
+        elif row.case_id in empty_cases:
+            assert row.available_variation_data == []
---
+++ tests/integration/utils/join_utils.py
@@ -0,0 +1,191 @@
+import functools
+import itertools
+from typing import (
+    AbstractSet,
+    Callable,
+    Iterable,
+    Iterator,
+    NamedTuple,
+    Tuple,
+    Union,
+    Mapping,
+)
+import more_itertools
+
+from pyspark import sql
+from pyspark.sql import functions as F
+
+
+RelationshipMapping = Mapping[str, Union[AbstractSet[str], "RelationshipMapping"]]
+
+
+class Relationship(NamedTuple):
+    parent_field: str
+    path_to_child: str
+    child_field: str
+
+
+def _follow_path(path: str) -> Callable[[sql.Row], Iterable[sql.Row]]:
+    if not path:
+        return lambda r: (r,)
+
+    def follow_path(row: sql.Row, path: str) -> Iterable[sql.Row]:
+        head_path, next_path = more_itertools.padded(path.split("."), n=2)
+        result = row[head_path]
+
+        if not next_path:
+            if isinstance(result, Iterable):
+                yield from result
+            else:
+                yield result
+        else:
+            if isinstance(result, Iterable):
+                yield from itertools.chain.from_iterable(
+                    follow_path(r, next_path) for r in result
+                )
+            else:
+                yield from follow_path(result, next_path)
+
+    return functools.partial(follow_path, path=path)
+
+
+def _relationship_reduce(
+    relationships: Iterator[Relationship],
+) -> Callable[
+    [Iterable[Iterable[sql.Row]]],
+    Union[AbstractSet[str], RelationshipMapping],
+]:
+    relationship = more_itertools.first(relationships)
+
+    return lambda groups: _get_relationship_map(
+        relationship, relationships, itertools.chain(*groups)
+    )
+
+
+def _get_relationship_map(
+    relationship: Relationship,
+    relationships: Iterator[Relationship],
+    rows: Iterable[sql.Row],
+) -> RelationshipMapping:
+    next_relationship = more_itertools.first(relationships, default=None)
+
+    if next_relationship:
+        relationships = more_itertools.prepend(next_relationship, relationships)
+
+        return more_itertools.map_reduce(
+            rows,
+            keyfunc=lambda r: r[relationship.parent_field],
+            valuefunc=_follow_path(relationship.path_to_child),
+            reducefunc=_relationship_reduce(relationships),
+        )
+
+    assert (
+        relationship.child_field
+    ), "Child field must be defined in the terminal relationship"
+
+    return more_itertools.map_reduce(
+        rows,
+        keyfunc=lambda r: r[relationship.parent_field],
+        valuefunc=_follow_path(relationship.path_to_child),
+        reducefunc=lambda rs: frozenset(
+            r[relationship.child_field] for r in itertools.chain(*rs)
+        ),
+    )
+
+
+def _get_relationships(path: Iterable[str]) -> Iterator[Relationship]:
+    def _split_path(path: str) -> Tuple[str, str]:
+        split = path.split(".")
+
+        return ".".join(split[:-1]), split[-1]
+
+    child_field = ""
+
+    for parent, child in more_itertools.pairwise(path):
+        parent_field = child_field or parent
+        path_to_child, child_field = _split_path(child)
+
+        yield Relationship(parent_field, path_to_child, child_field)
+
+
+def get_relationship_map(
+    data: Union[sql.DataFrame, Iterable[sql.Row]], relationship_path: Iterable[str]
+) -> RelationshipMapping:
+    """
+    Builds a mapping of a parent object's ID to all of its childrens IDs found in the
+    rows of the given data frame.
+
+    Args:
+        data: Either a dataframe or an iterable of rows which contain the given
+            relationship path.
+        relationship_path: A series of string values which all represent a parent child
+            relationship where the first element is the parent, second child, third
+            grandchild and etc. The parent field will be the key in the resulting
+            RelationshipMapping and the value will be all associated child field values,
+            either as keys in their own sub-RelationshipMapping or as values in a Set.
+
+            Note: all child fields can be a path through structures but the terminal key
+                must relate to a scalar value in the data.
+                e.g. `this.is.in.a.nested.object.obj_id`: this would reteive all child
+                obj_ids values found nested object.
+
+    Returns:
+        A mapping in which the key is the ID of the parent object and the values is a
+        set of all unique child IDs associated with the parent object.
+    """
+    relationships = _get_relationships(relationship_path)
+    relationship = more_itertools.first(relationships)
+
+    if isinstance(data, sql.DataFrame):
+        data = data.toLocalIterator()
+
+    return _get_relationship_map(relationship, relationships, data)
+
+
+def unpack_df_list(
+    dataframe: sql.DataFrame,
+    parent_fields: Union[str, Iterable[str]],
+    list_field: str,
+    packed_fields: Union[str, Iterable[str]],
+) -> sql.DataFrame:
+    """
+    Explodes packed into a list fields in :dataframe
+    Returns flat dataframe with only :parent_fields and :packed_fields
+
+    Example:
+        Given dataframe of format:
+            ssm{}
+                |___ ssm_id
+                |___ consequence[]
+                            |___consequence_id
+
+        unpack_df_join(
+            dataframe, 'ssm_id', 'consequence', 'consequence_id'
+        ) will return flat dataframe || ssm_id | consequence_id ||
+
+    NOTE: this works with multiple :parent_fields and :packed_fields too,
+        e.g.:
+        unpack_df_join(
+            dataframe, ['ssm_id', 'foo'],
+            'consequence', ['consequence_id', 'bar']
+        ) will return flat dataframe
+                                || ssm_id | foo | consequence_id | bar ||
+
+    """
+    if isinstance(parent_fields, str):
+        parent_fields = (parent_fields,)
+
+    if isinstance(packed_fields, str):
+        packed_fields = (packed_fields,)
+
+    exploded_alias = list_field.split(".")[-1]
+    child_fields = (f"{exploded_alias}.{f}" for f in packed_fields)
+
+    # this split allows deeper parent fields like "foo.bar"
+    aliases = tuple(f.split(".")[-1] for f in parent_fields)
+    all_fields = itertools.chain(aliases, child_fields)
+
+    return dataframe.select(
+        F.explode(list_field).alias(exploded_alias),
+        *(F.col(f).alias(a) for f, a in zip(parent_fields, aliases)),
+    ).select(*all_fields)
