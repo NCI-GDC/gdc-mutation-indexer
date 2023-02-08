0a6e0577aaedd17ed3b673c752754a66208f8889
Qiao Qiao <qiaoq@uchicago.edu>
2023-02-08 14:47:24 -0600

    Update dependencies indexclient to 2.3.2

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
         "typing-extensions~=4.1",
         "gdcdictionary @ git+https://github.com/NCI-GDC/gdcdictionary.git@2.4.0#egg=gdcdictionary",
         "gdcdatamodel @ git+https://github.com/NCI-GDC/gdcdatamodel.git@3.4.0#egg=gdcdatamodel",
-        "indexclient @ git+https://github.com/NCI-GDC/indexclient.git@2.3.2-rc.0#egg=indexclient",
+        "indexclient @ git+https://github.com/NCI-GDC/indexclient.git@2.3.2#egg=indexclient",
         "gdcmodels @ git+ssh://git@github.com/NCI-GDC/gdc-models.git@2.10.0-rc.2#egg=gdcmodels",
         "normalizer @ git+ssh://git@github.com/NCI-GDC/normalizer.git@2.1.1#egg=normalizer",
         "mutationindexerresource @ git+ssh://git@github.com/NCI-GDC/mutation-indexer-resource.git@civic_annot#egg=mutationindexerresource",
