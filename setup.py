0156d0ddaf9ba3e0093c7c5cbd8e8501b2ba8c66
Michael Byrnes <byrnesmi@uchicago.edu>
2023-07-03 08:28:54 -0500

    DEV-1752: Fix defaulted settings from normalizer.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         "gdcdatamodel @ git+https://github.com/NCI-GDC/gdcdatamodel.git@3.4.0#egg=gdcdatamodel",
         "indexclient @ git+https://github.com/NCI-GDC/indexclient.git@2.3.2#egg=indexclient",
         "gdcmodels @ git+ssh://git@github.com/NCI-GDC/gdc-models.git@3.2.1#egg=gdcmodels",
-        "normalizer @ git+ssh://git@github.com/NCI-GDC/normalizer.git@2.1.1#egg=normalizer",
+        "normalizer @ git+ssh://git@github.com/NCI-GDC/normalizer.git@feat/dev-1752-fix-setting-default-settings#egg=normalizer",
         "mutationindexerresource @ git+ssh://git@github.com/NCI-GDC/mutation-indexer-resource.git@civic_annot#egg=mutationindexerresource",
     ],
 )
