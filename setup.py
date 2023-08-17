fa718ce66c014736729858ca2cb0b85007e2d3ea
Michael Byrnes <byrnesmi@uchicago.edu>
2023-08-17 13:44:39 -0500

    DEV-1987: Update gdc-models version.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         "gdcdictionary @ git+https://github.com/NCI-GDC/gdcdictionary.git@2.4.0#egg=gdcdictionary",
         "gdcdatamodel @ git+https://github.com/NCI-GDC/gdcdatamodel.git@3.4.0#egg=gdcdatamodel",
         "indexclient @ git+https://github.com/NCI-GDC/indexclient.git@2.3.2#egg=indexclient",
-        "gdcmodels @ git+ssh://git@github.com/NCI-GDC/gdc-models.git@4.0.0-rc.1#egg=gdcmodels",
+        "gdcmodels @ git+ssh://git@github.com/NCI-GDC/gdc-models.git@4.0.0#egg=gdcmodels",
         "normalizer @ git+ssh://git@github.com/NCI-GDC/normalizer.git@4.0.0-rc.1#egg=normalizer",
         "mutationindexerresource @ git+ssh://git@github.com/NCI-GDC/mutation-indexer-resource.git@civic_annot#egg=mutationindexerresource",
     ],
