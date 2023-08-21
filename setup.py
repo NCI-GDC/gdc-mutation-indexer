7406c11a28deba25e57d44f4ae4ecbdc9e7574ec
Michael Byrnes <byrnesmi@uchicago.edu>
2023-08-21 13:27:32 -0500

    DEV-1987: Update pins.

--- setup.py
+++ setup.py
@@ -33,8 +33,8 @@ setup(
         "gdcdictionary @ git+https://github.com/NCI-GDC/gdcdictionary.git@2.4.0#egg=gdcdictionary",
         "gdcdatamodel @ git+https://github.com/NCI-GDC/gdcdatamodel.git@3.4.0#egg=gdcdatamodel",
         "indexclient @ git+https://github.com/NCI-GDC/indexclient.git@2.3.2#egg=indexclient",
-        "gdcmodels @ git+ssh://git@github.com/NCI-GDC/gdc-models.git@4.0.0#egg=gdcmodels",
-        "normalizer @ git+ssh://git@github.com/NCI-GDC/normalizer.git@4.0.0-rc.1#egg=normalizer",
-        "mutationindexerresource @ git+ssh://git@github.com/NCI-GDC/mutation-indexer-resource.git@civic_annot#egg=mutationindexerresource",
+        "gdcmodels @ git+https://github.com/NCI-GDC/gdc-models.git@4.0.0#egg=gdcmodels",
+        "normalizer @ git+https://github.com/NCI-GDC/normalizer.git@4.0.0#egg=normalizer",
+        "mutationindexerresource @ git+https://github.com/NCI-GDC/mutation-indexer-resource.git@civic_annot#egg=mutationindexerresource",
     ],
 )
