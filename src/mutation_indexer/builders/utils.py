25affaf3824d8dc2099cc8f261aac2a1e1c12ba4
Michael Byrnes <byrnesmi@uchicago.edu>
2023-09-08 10:11:50 -0500

    DEV-1250: Use simpler logic for decimal conversion.

--- src/mutation_indexer/builders/utils.py
+++ src/mutation_indexer/builders/utils.py
@@ -358,9 +358,7 @@ def filter_arrays_by_relative_size(
 
     # Standardizes the given percentile to an equivalent decimal value
     # w/ a precision of 2 decimal places. E.g. 99 -> 0.99
-    percentile_decimal = decimal.Decimal(size_percentile / 100).quantize(
-        decimal.Decimal("1.00")
-    )
+    percentile_decimal = size_percentile / decimal.Decimal("100.00")
     percentile_window = sql.Window.orderBy("_size")
     df = df.select("*", F.size(array_field).alias("_size")).select(
         "*", F.percent_rank().over(percentile_window).alias("_percentile")
