6a85c6c2fca6a03ec2678bb0c2adcb432266c57e
Micky Byrnes <68259544+BenX00@users.noreply.github.com>
2023-09-18 10:42:06 -0500

    DEV-1256: Move the base builders truncation logic to utils. (#429)

--- src/mutation_indexer/builders/base_builder.py
+++ src/mutation_indexer/builders/base_builder.py
@@ -9,7 +9,7 @@ from pyspark.sql.functions import col, size
 from pyspark.sql.types import ArrayType, BooleanType, MapType, StructType
 from typing_extensions import Self
 
-from mutation_indexer.builders.utils import percentile
+from mutation_indexer.builders import utils
 from mutation_indexer.constants import app
 
 logging.basicConfig(format=app.LOG_FORMAT)
@@ -163,47 +163,14 @@ class BaseBuilder:
         df_to_truncate,
         field,
         percentile_threshold,
-        df_for_percentile_calculation=None,
     ):
         """
         Truncates df_to_truncate to remove rows
         where field > percentile_threshold
         """
-
-        if percentile_threshold < 100:
-            self.log("Calculating number of {}".format(field))
-
-            count_col_name = "{}_count".format(field.replace(".", "_"))
-            df_to_truncate = df_to_truncate.withColumn(count_col_name, size(col(field)))
-            if df_for_percentile_calculation is None:
-                df_for_percentile_calculation = df_to_truncate
-            else:
-                df_for_percentile_calculation = df_for_percentile_calculation.withColumn(
-                    count_col_name, size(col(field))
-                )
-
-            self.log("Calculating {} percentile".format(percentile_threshold))
-            threshold = percentile(
-                [
-                    int(r[count_col_name])
-                    for r in df_for_percentile_calculation.select(
-                        count_col_name
-                    ).collect()
-                ],
-                percentile_threshold,
-            )
-
-            self.log(
-                "Truncating dataframe"
-                "(removing rows where number of "
-                "{} > {})".format(field, threshold)
-            )
-
-            df_to_truncate = df_to_truncate.filter(
-                "{} <= {}".format(count_col_name, threshold)
-            ).drop(count_col_name)
-
-        return df_to_truncate
+        return utils.filter_arrays_by_relative_size(
+            df_to_truncate, field, percentile_threshold
+        )
 
     def load_raw(self, path=None):
         """
