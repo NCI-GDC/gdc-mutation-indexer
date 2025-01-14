bc2aa69395caac3e9619a0ca2ff91f1cf564fdf2
jiakf <jkfeng@uchicago.edu>
2025-01-14 10:39:54 -0600

    try adding bindaddress to spark session in integration tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -157,6 +157,8 @@ def spark_session() -> Generator[sql.SparkSession, None, None]:
         "spark.ui.enabled", False
     ).config(
         "spark.driver.memory", "2g"
+    ).config(
+        "spark.driver.bindAddress", "127.0.0.1"
     ).getOrCreate() as spark_session:
         spark_session.sparkContext.setLogLevel("FATAL")
         spark_session.sql("set spark.sql.caseSensitive=true")
