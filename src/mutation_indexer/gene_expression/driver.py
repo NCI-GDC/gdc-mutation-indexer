d0739fda4845bd64a31e96088f3cce1809cfcc47
Michael Byrnes <byrnesmi@uchicago.edu>
2025-04-24 12:22:55 -0500

    DEV-3353: fix db scope.

--- src/mutation_indexer/gene_expression/driver.py
+++ src/mutation_indexer/gene_expression/driver.py
@@ -58,10 +58,14 @@ class Driver(driver.Driver[configuration.Configuration]):
             A context manager wrapping the dependencies. The context should be exited
             only after the dependent builders and done being used.
         """
-        with driver.get_es_client(config.elasticsearch.connection) as es_client:
+        s3_client = _initialize_s3_client(config.aws.s3)
+
+        with (
+            driver.get_es_client(config.elasticsearch.connection) as es_client,
+            sqlite.SQLiteDatabase(config.sqlite_database, s3_client) as db,
+        ):
             index_client = driver.get_index_client(config.indexd)
             mappings_loader = es_utils.MappingsLoader()
-            s3_client = _initialize_s3_client(config.aws.s3)
 
             yield Dependencies(
                 indexd_utils.DataFrameUtil(
@@ -79,7 +83,7 @@ class Driver(driver.Driver[configuration.Configuration]):
                 mappings_loader,
                 s3_client,
                 spark_session,
-                sqlite.SQLiteDatabase(config.sqlite_database, s3_client),
+                db,
             )
 
     def _builders(
